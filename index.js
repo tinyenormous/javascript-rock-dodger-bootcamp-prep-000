471cbf5b98baa6e12a66ce871247fac81fd70344
ross daly <rossdaly@gmail.com>
2018-09-25 17:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -146,9 +146,11 @@ function moveDodger(e) {
     if (e.which === LEFT_ARROW){
       moveDodgerLeft();
       e.preventDefault()
+      e.stopPropagation()
     } else if (e.which === RIGHT_ARROW){
       moveDodgerRight();
       e.preventDefault()
+      e.stopPropagation()
     }
   
 }
